cd72afb01c25dd08c932fb411de5bb8121cb6f65
Maxene <maxene.graze@gmail.com>
2022-02-04 07:44:23 -0800

    trying to revert

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-auto'
+import vercel from '@sveltejs/adapter-vercel'
 import path from 'path'
 import preprocess from 'svelte-preprocess'
 
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: vercel(),
     // hydrate the <div id="svelte"> element in src/app.html
     vite: {
       ssr: {
