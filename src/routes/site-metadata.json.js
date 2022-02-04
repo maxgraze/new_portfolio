c69bffa69fcca694107484b24eb4e3efce9e472b
Maxene <maxene.graze@gmail.com>
2022-02-04 09:02:41 -0800

    trying another

--- src/routes/site-metadata.json.js
+++ src/routes/site-metadata.json.js
@@ -1,31 +1,46 @@
 import { client } from '$lib/graphql-client'
 import { gql } from 'graphql-request'
 
-const get = async () => {
-  try {
-    const query = gql`
-      query GetProjectMetadatas {
-        projectMetadatas {
-          name
-          siteUrl
-          description
-        }
+// const get = async () => {
+//   try {
+//     const query = gql`
+//       query GetProjectMetadatas {
+//         projectMetadatas {
+//           name
+//           siteUrl
+//           description
+//         }
+//       }
+//     `
+//     const { projectMetadatas } = await client.request(query)
+
+//     return {
+//       status: 200,
+//       body: projectMetadatas[0],
+//     }
+//   } catch (error) {
+//     return {
+//       status: 500,
+//       body: { error: error.message },
+//     }
+//   }
+// }
+
+// get().catch(err => null)
+
+// export default get
+
+export const get = (async function () {
+  const query = gql`
+    query GetProjectMetadatas {
+      projectMetadatas {
+        name
+        siteUrl
+        description
       }
-    `
-    const { projectMetadatas } = await client.request(query)
-
-    return {
-      status: 200,
-      body: projectMetadatas[0],
     }
-  } catch (error) {
-    return {
-      status: 500,
-      body: { error: error.message },
-    }
-  }
-}
-
-get().catch(err => null)
-
-export default get
+  `
+  return ({ projectMetadatas } = await client.request(query))
+})().catch(e => {
+  console.error(e)
+})
