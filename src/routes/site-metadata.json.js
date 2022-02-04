83ce8261ea22ea9d4e449c051601daa37e093bd7
Maxene <maxene.graze@gmail.com>
2022-02-04 09:13:25 -0800

    saving absolute url

--- src/routes/site-metadata.json.js
+++ src/routes/site-metadata.json.js
@@ -1,46 +1,46 @@
 import { client } from '$lib/graphql-client'
 import { gql } from 'graphql-request'
 
-// const get = async () => {
-//   try {
-//     const query = gql`
-//       query GetProjectMetadatas {
-//         projectMetadatas {
-//           name
-//           siteUrl
-//           description
-//         }
-//       }
-//     `
-//     const { projectMetadatas } = await client.request(query)
+export const get = async () => {
+  try {
+    const query = gql`
+      query GetProjectMetadatas {
+        projectMetadatas {
+          name
+          siteUrl
+          description
+        }
+      }
+    `
+    const { projectMetadatas } = await client.request(query)
 
-//     return {
-//       status: 200,
-//       body: projectMetadatas[0],
-//     }
-//   } catch (error) {
-//     return {
-//       status: 500,
-//       body: { error: error.message },
-//     }
-//   }
-// }
+    return {
+      status: 200,
+      body: projectMetadatas[0],
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      body: { error: error.message },
+    }
+  }
+}
 
 // get().catch(err => null)
 
 // export default get
 
-export const get = (async function () {
-  const query = gql`
-    query GetProjectMetadatas {
-      projectMetadatas {
-        name
-        siteUrl
-        description
-      }
-    }
-  `
-  return ({ projectMetadatas } = await client.request(query))
-})().catch(e => {
-  console.error(e)
-})
+// export const get = (async function () {
+//   const query = gql`
+//     query GetProjectMetadatas {
+//       projectMetadatas {
+//         name
+//         siteUrl
+//         description
+//       }
+//     }
+//   `
+//   return ({ projectMetadatas } = await client.request(query))
+// })().catch(e => {
+//   console.error(e)
+// })
